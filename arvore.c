2e7240d09b858d97c31dda4b5c9ed142ce8e6af5
Davi0101 <dddvid65@gmail.com>
2024-10-27 20:32:12 -0300

    manipulacao de arvore

--- arvore.c
+++ arvore.c
@@ -1,12 +1,48 @@
-#include <stdio.h>
-#include <stdio.h>
+
 #include "arvore.h"
 
 
 
 
-int main(int argc, char const *argv[])
+int main()
 {
-    
+    int opcao;
+    int val;
+    No *arvore = NULL;
+    do
+    {
+        printf("\nDigite uma operacao: \n1-para inserir um elemento \n2-para imprimir arvore \n3-para imprimir preordenado \n4- para imprimir posordenado\n0-para sair\n");
+        scanf("%d",&opcao);
+
+        if (opcao == 1)
+        {
+            printf("\nDigite o numero a ser inserido: ");
+            scanf("%d",&val);
+            arvore = inserir_ArvB(val, arvore);
+            
+        }
+        else if (opcao == 2)
+        {
+            print_ordenado(arvore);
+            
+        }
+        else if (opcao == 3)
+        {
+            print_pre_ordenado(arvore);
+            
+        }
+        else if (opcao == 4)
+        {
+            print_pos_ordenado(arvore);
+            
+        }       
+        else if (opcao != 0)
+        {
+            printf("Opcao invalida::::\n");
+        }
+        
+        
+    } while (opcao != 0);
+        
     return 0;
 }
--- arvore.h
+++ arvore.h
@@ -1,12 +1,78 @@
 #ifndef arvore_h
 #define arvore_h
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct arv
+{
+    int info;
+    struct arv *dir;
+    struct arv *esq;
+
+}No;
+
+
+No *inserir_ArvB(int info, No *Raiz)
+{
+    if(Raiz == NULL){
+        No *novo = (No*)malloc(sizeof(No));
+        novo->info = info;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        return novo;
+
+    }
+    else
+    {
+        if (info < Raiz->info )
+        {
+            Raiz->esq = inserir_ArvB(info, Raiz->esq);
+        }
+        else
+        {
+            Raiz->dir = inserir_ArvB(info, Raiz->dir);
+        }
+    }
+    
+    return Raiz;
+}
+
+void print_ordenado(No *Raiz){
+    if (Raiz!= NULL)
+    {
+        print_ordenado(Raiz->esq);
+        printf("%d ",Raiz->info);
+        print_ordenado(Raiz->dir);
+
+    }
+    
+}
+void print_pre_ordenado(No *Raiz){
+    if (Raiz!= NULL)
+    {
+        printf("%d ",Raiz->info);
+        print_ordenado(Raiz->esq);
+        print_ordenado(Raiz->dir);
+    }
+    
+}
+void print_pos_ordenado(No *Raiz){
+    if (Raiz!= NULL)
+    {
+        print_ordenado(Raiz->esq);
+        print_ordenado(Raiz->dir);
+        printf("%d ",Raiz->info);
+
+    }
+    
+}
 
 
 
 
 
-
-
-#include "arvore.c"
+//#include "arvore.c"
 #endif
 
