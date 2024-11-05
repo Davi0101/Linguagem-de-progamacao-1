1169ad452804eb302af504e5f2fced69e092ea04
Davi0101 <dddvid65@gmail.com>
2024-11-05 06:10:04 -0300

    questao_3

--- Beecrowd_1466.c
+++ Beecrowd_1466.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#define MAX 1000 
 
 typedef struct arv {
-    char info;
+    int info;
     struct arv *dir;
     struct arv *esq;
 } No;
 
-No *inserir_ArvB(char info, No *Raiz);
-void print_nivel(No *Raiz);
+No *inserir_ArvB(int info, No *Raiz);
+void percurso_nivel(No *Raiz);
+void liberar_arvore(No *raiz);
+void enfileirar(No *no);
+No *desenfileirar();
+No *fila[MAX];
+
+int frente = 0, tras = 0;
 
 int main() {
-    char operacao[8];
-    char info;
-    No *arvore = NULL;
+    int C;
+    scanf("%d", &C); 
+
+    for (int caso = 1; caso <= C; caso++) {
+
+        frente = 0;
+        tras = 0;
+
+        int N;
+        scanf("%d", &N); 
+        No *arvore = NULL;
+
+        for (int i = 0; i < N; i++) {
+            int valor;
+            scanf("%d", &valor); 
+            arvore = inserir_ArvB(valor, arvore);
+        }
+
+        printf("Case %d:\n", caso);
+        percurso_nivel(arvore); 
+        printf("\n\n");
+            
+        
+    }
+
     return 0;
 }
 
-No *inserir_ArvB(char info, No *Raiz) {
+No *inserir_ArvB(int info, No *Raiz) {
     if (Raiz == NULL) {
         No *novo = (No *)malloc(sizeof(No));
         novo->info = info;
@@ -35,6 +63,40 @@ No *inserir_ArvB(char info, No *Raiz) {
     return Raiz;
 }
 
-void print_nivel(No *Raiz){
+void enfileirar(No *no) {
+    if (tras < MAX) { 
+        fila[tras++] = no;
+    }
+}
+
+No *desenfileirar() {
+    return fila[frente++];
+}
+
+void percurso_nivel(No *Raiz) {
+    if (Raiz == NULL) {
+        return;
+    }
+
+    enfileirar(Raiz);
+    int primeiro = 1;
+
+    while (frente < tras) {
+        No *noAtual = desenfileirar();
+        if (!primeiro) {
+            printf(" "); 
+        } else {
+            primeiro = 0;  
+        }
+        printf("%d", noAtual->info);
+        
+        if (noAtual->esq != NULL) {
+            enfileirar(noAtual->esq); 
+        }
+        if (noAtual->dir != NULL) {
+            enfileirar(noAtual->dir); 
+        }
+    }
     
 }
+
